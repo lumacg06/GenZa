7a8b4bbc0402ea70d58cd6651a2c744b93a5a6b5
Luisa Chavarría <156725126+lumacg06@users.noreply.github.com>
2024-12-06 01:22:34 -0500

    FiribaseService.java modificado "Declaración de paquete fija, punto y coma agregado y código reformateado en FiribaseService.java

--- backend/src/main/java/com/genza/backend/service/FiribaseService.java
+++ backend/src/main/java/com/genza/backend/service/FiribaseService.java
@@ -1,16 +1,15 @@
-package com.genza.backend.service
+package com.genza.backend.service;
 
 import org.springframework.stereotype.Service;
 
-@service
-class FibisaseService{
+@Service
+class FibisaseService {
 
-    FileInputStream serviceAccount =
-new FileInputStream("path/to/serviceAccountKey.json");
+    FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountKey.json");
 
-FirebaseOptions options = new FirebaseOptions.Builder()
-  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-  .build();
+    FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build();
 
-FirebaseApp.initializeApp(options);
+    FirebaseApp.initializeApp(options);
 }
